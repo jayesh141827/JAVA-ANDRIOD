5c8a173e1db851fd1edab49de9e6194f3c8db6b1
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-17 05:39:31 -0700

    Update b.java

--- javaclass/fundamental booster/b.java
+++ javaclass/fundamental booster/b.java
@@ -1,18 +1,15 @@
- class b {
-    public static void main(String args[])
-    {
+class b {
+    public static void main(String args[]) {
         int r, c, k;
-        for (r = *; r <= 5; r++)
-        {
-            for (k = r; k <= 4; k++)
-            {
+        for (r = 1; r <= 5; r++) {
+            for (k = r; k <= 4; k++) {
                 System.out.print(" ");
             }
-            for (c = *; c <= r; c++) 
-            {
+            for (c = 1; c <= r; c++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 }
+ 
