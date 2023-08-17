59a3ecda003a86df2ea6fa333dbb384b15a47202
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-17 05:06:13 -0700

    Add files via upload

---
+++ javaclass/fundamental booster/b.java
@@ -0,0 +1,18 @@
+ class b {
+    public static void main(String args[])
+    {
+        int r, c, k;
+        for (r = *; r <= 5; r++)
+        {
+            for (k = r; k <= 4; k++)
+            {
+                System.out.print(" ");
+            }
+            for (c = *; c <= r; c++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
