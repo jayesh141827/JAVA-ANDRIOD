3342d29d60faf50267bfc3c718066cd20295f2cb
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-17 08:53:33 -0700

    Update b.java

--- javaclass/fundamental booster/b.java
+++ javaclass/fundamental booster/b.java
@@ -1,12 +1,18 @@
-class b {
-    public static void main(String args[]) {
+class Pattern(b) 
+{
+    public static void main(String args[])
+   {
+       
         int r, c, k;
-        for (r = 1; r <= 5; r++) {
-            for (k = r; k <= 4; k++) {
+        for (r = 1; r <= 5; r++)
+       {
+            for (k = r; k <= 4; k++)
+           {
                 System.out.print(" ");
             }
-            for (c = 1; c <= r; c++) {
-                System.out.print("*");
+            for (c = 1; c <= r; c++)
+            {
+                System.out.print(" *");
             }
             System.out.println();
         }
