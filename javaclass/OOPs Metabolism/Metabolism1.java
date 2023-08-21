5f8b2da5ff9a8d8d56a77bf278de0f66e391020f
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-21 18:52:13 +0530

    Update Metabolism1.java

--- javaclass/OOPs Metabolism/Metabolism1.java
+++ javaclass/OOPs Metabolism/Metabolism1.java
@@ -1,15 +1,38 @@
- class Metabolism1
- {
-    public static void main(String[] args)
-    {
-        Book defaultBook = new Book();
-        defaultBook.setBookId(1);
-        defaultBook.setTitle("Default Book");
-        defaultBook.setAuthor("Default Author");
-        Book paramBook = new Book(2, "Parameterized Book", "Parameterized Author");
+ public class Book {
+    // Data members
+    private int book_id;
+    private String title;
+    private String author;
 
-        System.out.println("Default Book: " + defaultBook.getTitle()+ " by " + defaultBook.getAuthor());
-        System.out.println("Parameterized Book: " + paramBook.getTitle() + " by " + paramBook.getAuthor());
+    // Default constructor
+    public Book() {
+        book_id = 0;
+        title = "Unknown";
+        author = "Unknown";
     }
 
+    // Parameterized constructor
+    public Book(int book_id, String title, String author) {
+        this.book_id = book_id;
+        this.title = title;
+        this.author = author;
+    }
+
+    // Main method to test the class
+    public static void main(String[] args) {
+        // Creating objects using both constructors
+        Book defaultBook = new Book();
+        Book parameterizedBook = new Book(1, "Java Programming", "John Doe");
+
+        // Displaying book details
+        System.out.println("Default Book Details:");
+        System.out.println("Book ID: " + defaultBook.book_id);
+        System.out.println("Title: " + defaultBook.title);
+        System.out.println("Author: " + defaultBook.author);
+
+        System.out.println("\nParameterized Book Details:");
+        System.out.println("Book ID: " + parameterizedBook.book_id);
+        System.out.println("Title: " + parameterizedBook.title);
+        System.out.println("Author: " + parameterizedBook.author);
+    }
 }
