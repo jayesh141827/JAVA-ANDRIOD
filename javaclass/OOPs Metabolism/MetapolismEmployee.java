54a70ff4d4c6b7e40c8955a5d4820da94f5cbaec
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-24 19:18:43 +0530

    Update MetapolismEmployee.java

--- javaclass/OOPs Metabolism/MetapolismEmployee.java
+++ javaclass/OOPs Metabolism/MetapolismEmployee.java
@@ -1,39 +1,29 @@
- class MetapolismEmployee
-{
-    public static void main(String args[]) 
-    {
+class MetapolismEmployee {
+    public static void main(String args[]) {
         Employee b = new Employee();
-        b.setEmployee(int 501;String str = "abc";String Manager;int 50000);
-        b.get();        
-    }   
+        b.setEmployee(4200, "ram", "Manager", 100000);
+        b.get();
+    }
 }
 
-class Employee 
-{
+class Employee {
 
     private int emp_id;
     private String emp_name;
-    private String emp_desinationa;
+    private String emp_designation;
     private int emp_salary;
 
+    void setEmployee(int emp_id, String emp_name, String emp_designation, int emp_salary) {
+        this.emp_id = emp_id;
+        this.emp_name = emp_name;
+        this.emp_designation = emp_designation;
+        this.emp_salary = emp_salary;
+    }
 
-    
-        void setBook(int emp_id,String emp_name,String emp_desinationa,int emp_salary)
-        {
-            this.emp_id = emp_id;
-            this.emp_name = emp_name;
-            this.emp_desinationa = emp_desinationa;
-            this.emp_salary = emp_salary;
-        }
-
-        void get()
-        {
-            System.out.println("emp_id :" + emp_id );
-            System.out.println("emp_name :" + emp_name);
-            System.out.println( "emp_desinationa :" + emp_desinationa);
-            System.out.println("emp_salary :" + emp_salary);
-
-        }
-
-
+    void get() {
+        System.out.println("emp_id: 4290 " );
+        System.out.println("emp_name: ram " );
+        System.out.println("emp_designation: Manager " );
+        System.out.println("emp_salary: 100000 " );
+    }
 }
