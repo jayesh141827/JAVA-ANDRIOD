1775894e616128e07ce00abab7891905d3754973
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-24 17:08:40 +0530

    Update Metabolism1.java

--- javaclass/OOPs Metabolism/Metabolism1.java
+++ javaclass/OOPs Metabolism/Metabolism1.java
@@ -1,37 +1,33 @@
-class Bookk 
+public class Metapolism
 {
-          int book_id;
-          String title;
-          String author;
-      
-          Book(){
-            int book = 0;
-            tital ="";
-            author ="";
-         }
-          
+    public static void main(String[] args) {
+        Book b = new Book();
+        b.setBook(1, "ABC", "XYZ"); 
+        b.get();      
+    }   
+}
 
+class Book {
 
-       Book(int id ,string booktital, string bookauthor) {
-
-        book_id = id;
-        tital = booktital;
-        author = bookauthor;
-    }
-    
-     
-       public static void main(String[] args) {
-        // Creating objects using constructors
-        Book defaultBook = new Book();  // Default constructor
-        Book paramBook = new Book(1, "The Great Gatsby", "F. Scott Fitzgerald");  // Parameterized constructor
-        
-        // Displaying information
-        System.out.println("Default Book - ID: " + defaultBook.book_id + ", Title: " + defaultBook.title + ", Author: " + defaultBook.author);
-        System.out.println("Parameterized Book - ID: " + paramBook.book_id + ", Title: " + paramBook.title + ", Author: " + paramBook.author);
-    }
-}  
+    private int book_id;
+    private String title;
+    private String author;
 
 
     
+        void setBook(int book_id,String title,String author)
+        {
+            this.book_id = book_id;
+            this.title = title;
+            this.author = author;
+        }
 
-    
+        void get()
+        {
+            System.out.println("book_id");
+            System.out.println("title");
+            System.out.println("author");
+        }
+
+
+}
