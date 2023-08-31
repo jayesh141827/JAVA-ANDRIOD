13039afbfc0b1417205709dd1995c312c0404afc
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-31 22:46:26 +0530

    Add files via upload

---
+++ javaclass/fundamental booster/Numberzero.java
@@ -0,0 +1,19 @@
+public class Numberzero{
+    
+    public static void main(String args[]){
+        
+        int zero = -10;
+        if (zero > 0)
+        {
+        System.out.println(zero+"is positive");
+        }
+        else if(zero == 0) 
+        {
+        System.out.println(zero+"is zero");
+        }
+        else 
+        {
+        System.out.println(zero+"is negative");
+        }
+    }
+};
