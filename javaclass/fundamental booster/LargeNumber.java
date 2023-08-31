e021b25d662a6d33288a14e30eb1ff4b5847630a
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-09-01 00:09:41 +0530

    Add files via upload

---
+++ javaclass/fundamental booster/LargeNumber.java
@@ -0,0 +1,22 @@
+public class LargeNumber{
+    
+    public static void main(String args[]){
+        
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        
+        int largest = a;
+        
+        if (b > largest) 
+        {
+        System.out.println(largest = b);
+        }
+        if (c > largest) 
+        {
+        System.out.println(largest = c);
+        }
+        System.out.println("The largest number is: "+ largest);
+        
+    }
+};
