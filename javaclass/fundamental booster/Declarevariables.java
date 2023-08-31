cb8f0ca5cb7622dd68dbbecabf6526b7c04829d7
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-31 20:24:16 +0530

    Add files via upload

---
+++ javaclass/fundamental booster/Declarevariables.java
@@ -0,0 +1,19 @@
+public class Declarevariables{
+   
+    public static void main(String args[]){
+       
+        int a = 10;
+        float b = 3.14f;
+        double c = 123.456;
+        long d = 1000000L;
+        char e  = 'A';
+        String f  = "Hello, World!";
+        
+        System.out.println("Ans a = "+a);
+        System.out.println("Ans b = "+b);
+        System.out.println("Ans c = "+c);
+        System.out.println("Ans d  = "+d);
+        System.out.println("Ans e  = "+e);
+        System.out.println("Ans f = "+f);
+    }
+}
