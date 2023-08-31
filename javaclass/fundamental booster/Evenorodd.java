805e853a8f6e4f08ee25f726f6addd1e1697aaf3
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-31 22:59:31 +0530

    Add files via upload

---
+++ javaclass/fundamental booster/Evenorodd.java
@@ -0,0 +1,16 @@
+public class Evenorodd{
+    
+    public static void main(String args[]){
+        
+        int number = 1 ;
+        
+        if (number % 2 == 0) 
+        {
+        System.out.println(number+"  is even");
+        }
+        else 
+        {
+        System.out.println(number+"  is odd");
+        }
+    }
+};
