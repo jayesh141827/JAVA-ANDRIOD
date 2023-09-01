745bf73aa83f298f305e4ea8df63248ed9083369
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-09-01 06:13:18 -0700

    Update DayorWeek.java

--- javaclass/fundamental booster/DayorWeek.java
+++ javaclass/fundamental booster/DayorWeek.java
@@ -1,45 +1,53 @@
-public class DayorWeek{
-    public static void main(String args[]){
-        
-        int day = 10; 
+public class DayorWeek {
+    public static void main(String args[]) {
+
+        int day = 1;
         String dayName;
-    
-    System.out.println("press 1 for Monday");
-    System.out.println("press 2 for Tuesday");
-    System.out.println("press 3 for Wednesday");
-    System.out.println("press 4 for Thursday");
-    System.out.println("press 5 for Friday");
-    System.out.println("press 6 for Saturday");
-    System.out.println("press 7 for Sunday");
-    System.out.println("\n");
-        
-        switch (day){
+
+        System.out.println("press 1 for Monday");
+        System.out.println("press 2 for Tuesday");
+        System.out.println("press 3 for Wednesday");
+        System.out.println("press 4 for Thursday");
+        System.out.println("press 5 for Friday");
+        System.out.println("press 6 for Saturday");
+        System.out.println("press 7 for Sunday");
+        System.out.println("\n");
+
+        switch (day) {
             case 1:
-                System.out.println(dayName ="Monday");
-                
+                        System.out.println("The day of the week is: " );
+
+                System.out.println("Monday");
+                break;
+
             case 2:
-                System.out.println(dayName = "Tuesday");
-            
+                System.out.println("Tuesday");
+                break;
+
             case 3:
-                System.out.println(dayName = "Wednesday");
-                
+                System.out.println("Wednesday");
+                break;
+
             case 4:
-                System.out.println(dayName = "Thursday");
-                
+                System.out.println("Thursday");
+                break;
+
             case 5:
-                System.out.println(dayName = "Friday");
-                
+                System.out.println("Friday");
+                break;
+
             case 6:
-                System.out.println(dayName = "Saturday");
-                
+                System.out.println("Saturday");
+                break;
+
             case 7:
-                System.out.println(dayName = "Sunday");
-                
+                System.out.println("Sunday");
+                break;
+
             default:
                 System.out.println(dayName = "Invalid day");
-                
+
         }
 
-        System.out.println("The day of the week is: " + dayName);
     }
 };
