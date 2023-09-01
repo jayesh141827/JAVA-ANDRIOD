1f9504314f1514b45c130fe5e5e8dc75c414b155
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-09-01 19:11:24 +0530

    Add files via upload

---
+++ javaclass/fundamental booster/SmallestNumber.java
@@ -0,0 +1,22 @@
+public class SmallestNumber {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        int c = 14;
+
+        int j;
+
+        if (a < b) {
+            System.out.println(j = a);
+        } else {
+            System.out.println(j = b);
+        }
+
+        if (j < c) {
+            System.out.println("The smallest number is: " + j);
+        } else {
+            System.out.println("The smallest number is: " + c);
+        }
+    }
+};
