9aec5aec91aecb193877741995f2fd9556bf20ee
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-09-06 06:19:05 -0700

    Add files via upload

---
+++ OOPs Metabolism/Employeetask1.java
@@ -0,0 +1,17 @@
+public class Employeetask1 {
+    public static void main(String args[]) {
+        class main {
+
+            int emp_id;
+            char emp_name;
+            char emp_designation;
+            int emp_salaEmployeery;
+
+        }
+
+        System.out.println("emp_id: 4290 ");
+        System.out.println("emp_name: ram ");
+        System.out.println("emp_designation: Manager ");
+        System.out.println("emp_salary: 100000 ");
+    }
+}
