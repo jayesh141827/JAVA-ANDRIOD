28da1925941ba9cb95eb928241b077d36f6d43fd
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-09-25 20:20:31 +0530

    Update Abstract2.java

--- Abstract Star/Main.java
+++ Abstract Star/Main.java
@@ -1,5 +1,58 @@
-public class Main{
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+import java.util.Scanner;
+
+public class Abstract2 {
+    public static void main(String[] args) {
+
+        SBI bank = new SBI();
+        bank.getRateOfInterest();
+        bank.setter();
+
+    }
+
+}
+
+abstract class Bank {
+    float Amount;
+    float PR;
+    float Month;
+    float TOtal Interest Rate;
+    final float Interest = 7.61f;
+    float K;
+    float Year;
+    Scanner scanner = new Scanner(System.in);
+
+    abstract public void getRateOfInterest();
+
+    abstract public void setter();
+
+}
+
+class SBI extends Bank {
+
+    public void getRateOfInterest() {
+
+        System.out.print("Enter of Month EMI = ");
+        Amount = scanner.nextInt();
+        System.out.print("Interest of yearly EMI = ");
+        K = scanner.nextFloat();
+        PR = Amount * Interest / 100;
+        Month = Amount + PR;
+        Year = Month * 12;
+        Total Interest Rate = PR * 12;
+        K = K * Year;
+
+    }
+
+    public void setter() {
+
+        System.out.println("This Month Interest Amount " + Month);
+        System.out.println("This year Tnterest Amount " + Year);
+        System.out.println("This year Total Tnterest " + Total Interest Rate);
+
+        System.out.println("--------------");
+
+        System.out.println("One year Loan Amount = " + K);
+
+    }
+
 	}
-};
